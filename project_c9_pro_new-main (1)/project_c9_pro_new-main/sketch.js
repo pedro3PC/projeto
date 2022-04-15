fb01880e98bc6c3cc3acb0e08b1635d488ec347c
pedro3PC <94692673+pedro3PC@users.noreply.github.com>
2022-04-15 11:48:19 -0300

    Add files via upload

---
+++ project_c9_pro_new-main (1)/project_c9_pro_new-main/sketch.js
@@ -0,0 +1,39 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+
+  if (keyDown("LEFT_ARROW")) 
+  {
+    background("blue");
+    
+  }
+ 
+  if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  { 
+    background("green");
+  
+  }
+
+  if (keyDown("RIGHT_ARROW")) 
+  {
+    background("red");
+  
+  }
+
+
+  drawSprites();
+}
